0076fde020daf754354285f1993bfecef8beac99
shelbylb <ksushauu97@gmail.com>
2025-12-04 16:57:25 +0800

    создает AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\DTO\RegisterDto;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Service\UserService;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class AuthController extends Controller
+{
+    public function __construct(
+        private readonly UserService $service
+    ) {}
+
+    /** Показ формы регистрации */
+    public function showRegistrationForm(): Factory|View
+    {
+        return view('auth.register');
+    }
+
+    /** Обработка регистрации */
+    public function register(RegisterRequest $request): RedirectResponse
+    {
+        $dto = RegisterDto::fromRequest($request);
+        $user = $this
+            ->userService
+            ->register($dto);
+
+        return redirect()
+            ->route('login.form')
+            ->with('status', 'Регистрация прошла успешно');
+    }
+}
